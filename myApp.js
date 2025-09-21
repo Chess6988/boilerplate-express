2570b80d3a9caed1167d35c893441fca0d58d9d9
ANONYMOUS <ANONYMOUS>
2025-09-21 05:03:22 +0100

    corrected expected error

--- myApp.js
+++ myApp.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const { v4: uuidv4 } = require('uuid');
+
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
